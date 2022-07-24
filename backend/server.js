cad689cbd0c075ba3df6d152918993ccc9c90ac3
Eshaan Amin <eshaanganesh2@gmail.com>
2022-07-24 22:39:02 +0530

    Server side code for storing files in the MongoDB database using Multer, GridFS-Stream and GridFS bucket; also consists of backend APIs that aid in the rendering of images and displaying of file content (for non-image files)

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,70 @@ const fetch = require('node-fetch');
 const { getMaxListeners } = require('process');
 var globalusername="";
 var flag=0;// User has not yet visited home page
+var crypto = require('crypto');
+//const trackRoute = express.Router();
+
 
 var mongodb = require('mongodb');
-
+var mongoose=require('mongoose');
 var MongoClient = mongodb.MongoClient;
 var url="mongodb://localhost:27017";
+var url2="mongodb://localhost:27017/UserDetails";
+
+const multer = require('multer');
+var Grid = require('gridfs-stream');
+const {GridFsStorage} = require('multer-gridfs-storage');
+var gfs;
+// console.log(conn);
+//conn.once('open', function () {
+    //Initialize stream
+    async function wrapper2(){
+    async function main(){
+        var conn = mongoose.createConnection(url2);
+        conn.once('open', function () {
+        gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+            bucketName: 'uploads'
+          })
+          gfs = Grid(conn.db, mongoose.mongo);
+          gfs.collection('uploads');
+        })
+    }
+    await sleep(5000);
+    await main();
+    }
+
+wrapper2();
+
+//})
+
+//Creating storage engine for files
+const storage = new GridFsStorage({
+    url: url2,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  const upload = multer({ storage });
 
 // For capturing the data submitted via the form
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 const bodyParser =require('body-parser');
+const { mongo } = require('mongoose');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('../public'));
@@ -165,10 +218,29 @@ app.get('/my_journal',async(req,res)=>{
     if(flag==1){
         const userdetails= await Register.findOne({username:globalusername});
         console.log(userdetails.notes);
-        return res.render('my_journal.ejs',{notes_user:userdetails.notes});
+        gfs.files.find().toArray((err,files)=>{
+            if(!files||files.length==0){
+                return res.render('my_journal.ejs',{notes_user:userdetails.notes,files:false});
+            }
+            else{
+                files.map(file=>{
+                    if(file.contentType=='image/jpeg'||file.contentType=='image/jpg'||file.contentType=='image/png'||file.contentType=='image/svg'){
+                        file.isImage=true;
+                    }
+                    else{
+                        file.isImage=false;
+                    }
+                });
+                return res.render('my_journal.ejs',{notes_user:userdetails.notes,files:files});
+            }
+        });
+        //return res.render('my_journal.ejs',{notes_user:userdetails.notes});
     }
-    globalusername="";
-    res.redirect('/login');
+    else{
+        globalusername="";
+        res.redirect('/login');
+    }
+    
 });
 
 // Handling POST request when user clicks 'save' for travel notes
@@ -203,7 +275,7 @@ app.post('/my_journal_posts',urlencodedParser,async(req,res)=>{
     const userdetails= await Register.findOne({username:globalusername});
     console.log(userdetails.notes);
     await sleep(2000);
-    res.render('my_journal.ejs',{notes_user:userdetails.notes});
+    res.redirect('/my_journal');
 })
 
 //Handling POST request when user clicks 'delete' button for a particular post
@@ -222,12 +294,64 @@ app.post('/my_journal_posts_delete',urlencodedParser,async(req,res)=>{
     );
     });
     await sleep(1000);
-    res.render('my_journal.ejs',{notes_user:userdetails.notes});
+    //res.render('my_journal.ejs',{notes_user:userdetails.notes});
+    res.redirect('/my_journal');
 })
 
-app.post('/my_journal_media',(req,res)=>{
-    res.send("media");
+app.post('/my_journal_media',upload.single('image'),(req,res)=>{
+    console.log(req);
+    MongoClient.connect(url,async function(err,client){
+        var db=client.db("UserDetails");
+        const y=db.collection("uploads.files").find();
+        console.log(y);
+        console.log(req.file.filename);
+        const x=await db.collection('uploads.files').updateOne({"filename":req.file.filename},{
+            $set: {
+                // "notes.id": "1",
+                "username": globalusername
+            }
+        });
+        console.log(x);
+        });
+        res.redirect('/my_journal');
 })
+
+app.get('/file/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      // File exists
+    //   return res.json(file);
+    const readStream = gridfsBucket.openDownloadStreamByName(file.filename);
+    readStream.pipe(res)
+    });
+  });
+
+app.get('/image/:filename',async(req,res)=>{
+    await gfs.files.findOne({filename:req.params.filename},(err,file)=>{
+        if(!file||file.length==0){
+            return res.status(404).json({
+                err:"No file exists"
+            });
+        }
+        console.log(file);
+        if(file.contentType=='image/jpeg'||file.contentType=='image/jpg'||file.contentType=='image/png'||file.contentType=='image/svg'){
+            // const readstream=gfs.createReadStream(file.filename);
+            // readstream.pipe(res);
+            const readStream = gridfsBucket.openDownloadStreamByName(file.filename);
+            readStream.pipe(res)
+        }
+        else{
+            res.status(404).json({
+                err:"Not an image file"
+            });
+        }
+    })
+});
 
 //User clicks on logout button
 app.post('/logout',(req,res)=>{
